0f9aeb12e37a2400d683ef03411375c5b07e64e1
Andi Sina <andy.sina32@gmail.com>
2023-11-28 15:24:55 +0100

    Added new and show

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -5,11 +5,22 @@ class JobsController < ApplicationController
   end
 
   def new
+    current_user
     @job = Job.new
   end
 
+  def show
+    @job = Job.find(params[:id])
+  end
+
   def create
-    @job = Job.create(job_params.merge(user: current_user))
+    @job = Job.new(job_params)
+    @job.user = current_user
+    if @job.save
+      redirect_to @job, notice: "Job was added was successfully created."
+    else
+      render :new, status: :unprocessable_entity
+    end
   end
 
   private
