27036c294b97dfc2fdd8036b64e904fe19e8c2a5
vanessa.x.marchiori@gmail.com <vanessa.x.marchiori@gmail.com>
2023-11-29 00:18:48 +0100

    started dashboard

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -2,4 +2,11 @@ class PagesController < ApplicationController
   def home
     # hello
   end
+
+  def dashboard
+    @my_bookings_requests = Booking.joins(:job).where(jobs: { user: current_user })
+    @my_bookings = Booking.where(user: current_user)
+    @my_jobs = Job.where(user: current_user)
+    @my_reviews = Review.where(booking: @my_bookings)
+  end
 end
