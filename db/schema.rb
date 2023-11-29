c543189c0cfc6d120b6e3ab9f0a34fc72ce37891
Anna Caffiero <annacaffiero@gmail.com>
2023-11-29 21:14:18 +0800

    Correction

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.1].define(version: 2023_11_28_155516) do
+ActiveRecord::Schema[7.1].define(version: 2023_11_29_131258) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -46,6 +46,7 @@ ActiveRecord::Schema[7.1].define(version: 2023_11_28_155516) do
     t.bigint "booking_id", null: false
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.text "content"
     t.index ["booking_id"], name: "index_reviews_on_booking_id"
   end
 
