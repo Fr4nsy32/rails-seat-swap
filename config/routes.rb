8217936c2c8b5a032258de221d10fc2bdd6b2e71
Anna Caffiero <annacaffiero@gmail.com>
2023-11-29 22:03:28 +0800

    Conflict fix

--- config/routes.rb
+++ config/routes.rb
@@ -20,4 +20,5 @@ Rails.application.routes.draw do
   resources :bookings, only: [] do
     resources :reviews, only: [:new, :create]
   end
+  patch "bookings/:id", to: "bookings#confirm", as: :confirm_bookings
 end
