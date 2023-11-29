24e69dd6354713be28c3fb7239d45b3a395b87f5
Anna Caffiero <annacaffiero@gmail.com>
2023-11-29 22:29:27 +0800

    Fix

--- app/models/job.rb
+++ app/models/job.rb
@@ -3,5 +3,6 @@ class Job < ApplicationRecord
   validates :price, :credit_to_earn, presence: true, numericality: { greater_than: 0 }
   validates :location, presence: true
   belongs_to :user
+  has_many :bookings
   # validates :avaible_from, presence: true
 end
