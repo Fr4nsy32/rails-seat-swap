789d5a40ca734401b7497a5a19465fe78b8656a0
vanessa.x.marchiori@gmail.com <vanessa.x.marchiori@gmail.com>
2023-11-30 14:10:11 +0100

    added cloudinary,and default pic user plus Arbi changes on cssss

--- app/models/job.rb
+++ app/models/job.rb
@@ -5,5 +5,6 @@ class Job < ApplicationRecord
   belongs_to :user
   has_many :bookings
   has_many :reviews, through: :bookings
+  has_one_attached :photo
   # validates :avaible_from, presence: true
 end
