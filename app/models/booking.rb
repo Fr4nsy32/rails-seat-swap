65ba015a7e6b3bbbbd8d716cc3e3160dba86acef
Andi Sina <andy.sina32@gmail.com>
2023-11-30 12:34:35 +0100

    Fixed typo

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -2,9 +2,5 @@ class Booking < ApplicationRecord
   belongs_to :job
   belongs_to :user
   has_many :reviews
-  enum :status, { pending: 0, booked: 1, cencelled: 2, completed: 3 }
-  # VALID_STATUSES = ['available', 'pending', 'booked', 'cancelled', 'completed'].freeze
-
-  # # Valider que le statut est inclus dans la liste des statuts valides
-  # validates :status, presence: true, inclusion: { in: VALID_STATUSES }
+  enum :status, { pending: 0, booked: 1, cancelled: 2, completed: 3 }
 end
