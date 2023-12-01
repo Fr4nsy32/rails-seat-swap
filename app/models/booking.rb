68ff64bc4efa6878acd3e761c153c7ed31ba1e02
Andi Sina <andy.sina32@gmail.com>
2023-12-01 11:46:44 +0100

    Dates displayed in a legible way

--- app/models/booking.rb
+++ app/models/booking.rb
@@ -3,4 +3,5 @@ class Booking < ApplicationRecord
   belongs_to :user
   has_many :reviews
   enum :status, { pending: 0, booked: 1, cancelled: 2, completed: 3 }
+  validates :start_date, :end_date, presence: true
 end
